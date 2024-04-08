3ba1011916f741e869de6bc54fa168708e17b036
qkdflrgs <qkdflrgs@gmail.com>
2024-04-08 18:50:33 +0900

    refactor(router): refactor App component(add Router component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import Router from "components/Router";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<h1>home</h1>} />
-      <Route path="/posts" element={<h1>Post</h1>} />
-      <Route path="/posts/new" element={<h1>New Post</h1>} />
-      <Route path="/posts/:id" element={<h1>Post Detail</h1>} />
-      <Route path="/posts/edit/:id" element={<h1>Edit Post</h1>} />
-      <Route path="/profile" element={<h1>Profile</h1>} />
-      <Route path="/profile/edit" element={<h1>Edit Profile</h1>} />
-      <Route path="/search" element={<h1>Search</h1>} />
-      <Route path="/notification" element={<h1>Notification</h1>} />
-      <Route path="/user/login" element={<h1>Login</h1>} />
-      <Route path="/user/signup" element={<h1>Signup</h1>} />
-      <Route path="*" element={<Navigate replace to="/" />} />
-    </Routes>
-  );
+  return <Router />;
 }
 
 export default App;
