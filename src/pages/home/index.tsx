337a32602f52a4490bd393e37f2c13ebd9319998
qkdflrgs <qkdflrgs@gmail.com>
2024-04-10 21:40:36 +0900

    feat(post): add onSnapshot to Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,9 @@
 import PostForm from "components/posts/PostForm";
 import PostBox from "components/posts/PostBox";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "context/AuthContext";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "firebaseApp";
 
 export interface PostProps {
   id: string;
@@ -13,9 +17,25 @@ export interface PostProps {
   comments?: any;
 }
 
-const post: PostProps[] = [];
-
 export default function HomePage() {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, "posts");
+      let postQuery = query(postsRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(postQuery, (snapshot) => {
+        const dataObj = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [user]);
+
   return (
     <div className="home">
       <div className="home__top">
@@ -27,9 +47,13 @@ export default function HomePage() {
       </div>
       <PostForm />
       <div className="post">
-        {post?.map((post) => (
-          <PostBox key={post.id} post={post} />
-        ))}
+        {posts.length > 0 ? (
+          posts?.map((post) => <PostBox key={post.id} post={post} />)
+        ) : (
+          <div className="post__no-posts">
+            <div className="post__text">게시글이 없습니다</div>
+          </div>
+        )}
       </div>
     </div>
   );
