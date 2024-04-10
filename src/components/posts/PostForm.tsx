55b162848645eb697742c40a8792741a8379e800
qkdflrgs <qkdflrgs@gmail.com>
2024-04-10 21:41:03 +0900

    feat(post): add PostForm component

--- src/components/posts/PostForm.tsx
+++ src/components/posts/PostForm.tsx
@@ -1,15 +1,55 @@
+import AuthContext from "context/AuthContext";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { FormEvent, useContext, useState } from "react";
 import { FiImage } from "react-icons/fi";
+import { toast } from "react-toastify";
 
 export default function PostForm() {
+  const [content, setContent] = useState<string>("");
+  const { user } = useContext(AuthContext);
   const handleFileUpload = () => {};
 
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await addDoc(collection(db, "posts"), {
+        content: content,
+        createdAt: new Date().toLocaleDateString("ko", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        }),
+        uid: user?.uid,
+        email: user?.email,
+      });
+      setContent("");
+      toast.success("게시글을 생성했습니다");
+    } catch (error: any) {
+      toast.error(error?.code);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "content") {
+      setContent(value);
+    }
+  };
+
   return (
-    <form className="post-form" action="">
+    <form className="post-form" onSubmit={onSubmit}>
       <textarea
         className="post-form__textarea"
         name="content"
         id="content"
         placeholder="What is happening?"
+        onChange={onChange}
+        value={content}
         required
       />
       <div className="post-form__submit-area">
