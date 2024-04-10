8108a0886991d26c0cb61be66c0627ef3e10a26b
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-04-10 19:54:59 +0900

    feat(auth): add Firebase Auth (#3)
    * feat(auth): install firebase
    
    * feat(auth): add .env to .gitignore
    
    * feat(auth): initialize firebase app
    
    * feat(auth): add firebase-auth to App
    
    * feat(auth): install react-toastify
    
    * feat(auth): add firebase auth observer to App
    
    * feat(auth): set AuthContext
    
    * feat(auth): add Loader component
    
    * feat(auth): add SignupForm component
    
    * feat(auth): add LoginForm component
    
    * refactor(auth): refactoring router for Authentication
    
    * refactor(auth): refactoring Menu for AuthContext
    
    * feat(auth): add form component
    
    * style(auth): add styles for form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import Layout from "components/Layout";
+import Loader from "components/Loader/Loader";
 import Router from "components/Router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "firebaseApp";
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth.currentUser
+  );
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      user ? setIsAuthenticated(true) : setIsAuthenticated(false);
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <Layout>
-      <Router />
+      <ToastContainer
+        theme="dark"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop
+      />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </Layout>
   );
 }
