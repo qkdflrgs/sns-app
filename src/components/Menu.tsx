4d2d8997b4886b6a5378137a79b60798397ab7d3
qkdflrgs <qkdflrgs@gmail.com>
2024-04-10 19:50:09 +0900

    refactor(auth): refactoring Menu for AuthContext

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,26 @@
 import { BsHouse } from "react-icons/bs";
 import { BiUserCircle } from "react-icons/bi";
-import { MdLogout } from "react-icons/md";
+import { MdLogin, MdLogout } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "context/AuthContext";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+import { toast } from "react-toastify";
 
 export default function Menu() {
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const auth = getAuth(app);
+      await signOut(auth);
+      toast.success("로그아웃 되었습니다");
+    } catch (error: any) {
+      toast.error(error?.code);
+    }
+  };
 
   return (
     <div className="footer">
@@ -17,10 +33,17 @@ export default function Menu() {
           <BiUserCircle />
           Profile
         </button>
-        <button type="button" onClick={() => navigate("/")}>
-          <MdLogout />
-          Logout
-        </button>
+        {user === null ? (
+          <button type="button" onClick={() => navigate("/user/login")}>
+            <MdLogin />
+            Login
+          </button>
+        ) : (
+          <button type="button" onClick={handleLogout}>
+            <MdLogout />
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
