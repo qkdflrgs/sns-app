71ddcff6878d0c2a3a4dd86ea5cf5c86f1688b34
qkdflrgs <qkdflrgs@gmail.com>
2024-04-12 15:04:23 +0900

    feat(comment): add comment function to post detail page

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,11 +1,13 @@
 import Loader from "components/Loader/Loader";
 import PostBox from "components/posts/PostBox";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "firebaseApp";
 import { PostProps } from "pages/home";
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PostHeader from "components/posts/PostHeader";
+import CommentForm from "components/comment/CommentForm";
+import CommentBox, { CommentProps } from "components/comment/CommentBox";
 
 export default function PostDetail() {
   const params = useParams();
@@ -14,20 +16,35 @@ export default function PostDetail() {
   const getPost = useCallback(async () => {
     if (params.id) {
       const docRef = doc(db, "posts", params.id);
-      const docSnap = await getDoc(docRef);
 
-      setPost({ ...(docSnap.data() as PostProps), id: docSnap.id });
+      onSnapshot(docRef, (doc) => {
+        setPost({ ...(doc?.data() as PostProps), id: doc.id });
+      });
     }
   }, [params.id]);
 
   useEffect(() => {
     if (params.id) getPost();
-  }, [getPost, params.id]);
+  }, [getPost, params.id, post?.likes]);
 
   return (
     <div className="post">
       <PostHeader />
-      {post ? <PostBox post={post} /> : <Loader />}
+      {post ? (
+        <>
+          <PostBox post={post} />
+          <CommentForm post={post} />
+          {post.comments &&
+            post.comments
+              .slice(0)
+              .reverse()
+              .map((data: CommentProps, index: number) => (
+                <CommentBox key={index} data={data} post={post} />
+              ))}
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
